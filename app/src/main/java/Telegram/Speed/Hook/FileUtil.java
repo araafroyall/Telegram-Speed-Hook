dce02803dd2b3ca3df8f74f6d7dd269873cf6340
AraafRoyall ❂ <swordigoar@gmail.com>
2025-06-22 12:21:09 +0530

    Update FileUtil.java

--- app/src/main/java/Telegram/Speed/Hook/FileUtil.java
+++ app/src/main/java/Telegram/Speed/Hook/FileUtil.java
@@ -2,4 +2,8 @@ package Telegram.Speed.Hook;
 
 public class FileUtil {
     
+    // Class by Araaf Royall
+    
+    // this class must be defined ( even emtpy ) if you working with sketchware pro
+    
 }
